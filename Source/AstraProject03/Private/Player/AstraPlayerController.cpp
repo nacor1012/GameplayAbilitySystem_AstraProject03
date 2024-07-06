f1b9cc4f54ce786ecb93760ea0a1019540f98b49
Adam <adamuldall101281@gmail.com>
2024-07-05 23:34:07 -1000

    Added input functionality to the Astra playercontroller no real idea what i am doing set some shit up for mouse commands for top down game'note added .H files that dick head never mentions to make shit work

--- Source/AstraProject03/Private/Player/AstraPlayerController.cpp
+++ Source/AstraProject03/Private/Player/AstraPlayerController.cpp
@@ -1,5 +1,29 @@
 // Copyright SkyHigh Gaming.
 
-
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
 #include "Player/AstraPlayerController.h"
 
+AAstraPlayerController::AAstraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAstraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(AstraContext);
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AstraContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+
+}
--- Source/AstraProject03/Public/Player/AstraPlayerController.h
+++ Source/AstraProject03/Public/Player/AstraPlayerController.h
@@ -6,12 +6,24 @@
 #include "GameFramework/PlayerController.h"
 #include "AstraPlayerController.generated.h"
 
-/**
- * 
- */
+class UInputMappingContext;
+
 UCLASS()
 class ASTRAPROJECT03_API AAstraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+
+	AAstraPlayerController();
+
+protected:
+
+	virtual void BeginPlay() override;
+
+private:
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputMappingContext> AstraContext;
+
 };
