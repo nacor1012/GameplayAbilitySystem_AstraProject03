400cc41db8abd716d4a7819aeabf8eca3b07c622
Adam <adamuldall101281@gmail.com>
2024-07-05 22:52:12 -1000

    moved player controller in player file to a proper player file added enhanced input to .cs file  messed with everything 100 times and fignally got enhanced inputs to work and still no idea what the fuck i did fuck you UE5

--- Source/AstraProject03/AstraProject03.Build.cs
+++ Source/AstraProject03/AstraProject03.Build.cs
@@ -8,7 +8,7 @@ public class AstraProject03 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "HairStrandsCore", "Niagara", "GeometryCollectionEngine", "UMG", "AIModule" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
