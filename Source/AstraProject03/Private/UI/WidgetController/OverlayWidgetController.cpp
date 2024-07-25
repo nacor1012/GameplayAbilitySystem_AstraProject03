7f2ce26fd924f912286376c6c291986c8e412220
Adam <adamuldall101281@gmail.com>
2024-07-24 22:06:01 -1000

    added message tags to rows in message widget data table then switched the tags in the potions to those and added a get data table row by tag template function to overlay controller.

--- Source/AstraProject03/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/AstraProject03/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -31,13 +31,15 @@ void UOverlayWidgetController::BindCallbacksToDependencies()
 	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AstraAttributeSet->GetMaxManaAttribute()).AddUObject(this, &UOverlayWidgetController::MaxManaChanged);
 
 	Cast<UAstraAbilitySystemComponent>(AbilitySystemComponent)->EffectAssetTags.AddLambda(
-		[](const FGameplayTagContainer& AssetTags) 
+		[this](const FGameplayTagContainer& AssetTags) 
 		{
 			for (const FGameplayTag& Tag : AssetTags)
 			{
 				//Converts converts the gameplay tag to a string the *before tag converts the string to an array to Tcars
 				const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
 				GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Blue, Msg);
+
+				FUIWidgetRow* Row = GetDataTableRowByTag<FUIWidgetRow>(MessageWidgetDataTable, Tag);
 			}
 		});
 
