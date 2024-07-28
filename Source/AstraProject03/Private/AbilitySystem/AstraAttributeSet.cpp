059718ff61754d63ddeb593738afc6d679fbb7cd
Adam <adamuldall101281@gmail.com>
2024-07-28 07:20:08 -1000

    Added a clamp inside of an if statement for health and mana in postproccess effect

--- Source/AstraProject03/Private/AbilitySystem/AstraAttributeSet.cpp
+++ Source/AstraProject03/Private/AbilitySystem/AstraAttributeSet.cpp
@@ -80,6 +80,15 @@ void UAstraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallb
 	FEffectProperties Props;
 	SetEffectProperties(Data, Props);
 
+	if (Data.EvaluatedData.Attribute == GetHealthAttribute())
+	{
+		SetHealth(FMath::Clamp(GetHealth(), 0.f, GetMaxHealth()));
+	}
+
+	if (Data.EvaluatedData.Attribute == GetManaAttribute())
+	{
+		SetMana(FMath::Clamp(GetMana(), 0.f, GetMaxMana()));
+	}
 
 }
 
